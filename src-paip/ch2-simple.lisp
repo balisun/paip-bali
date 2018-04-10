5092ddbc7f12f09b2a9c517852f1ce26f09dd01b
Bali Hsu <baliuzeger@gmail.com>
2018-04-10 12:05:33 +0800

    move exercises codes into chap2.lisp from ch2-simple.lisp

--- src-paip/ch2-simple.lisp
+++ src-paip/ch2-simple.lisp
@@ -87,32 +87,6 @@
          (generate (random-elt (rewrites phrase))))
         (t (list phrase))))
 
-
-;;; Exercise 2.1
-
-(defun generate (phrase)
-  "Generate a random sentence or phrase"
-  (let ((written-phrases))
-    (cond ((listp phrase)
-           (mappend #'generate phrase))
-          ((setf written-phrases (rewrites phrase))
-           (generate (random-elt written-phrases)))
-          (t (list phrase)))))
-
-;;; Exercise 2.2
-
-(defun generate (phrase)
-  "Generate a random sentence or phrase"
-  (let ((written-phrases))
-    (cond ((listp phrase)
-           (mappend #'generate phrase))
-          ((null (setf written-phrases (rewrites phrase)))
-           (list phrase))
-          (written-phrases
-           (generate (random-elt written-phrases))))))
-
-
-
 ;;; ==============================
 
 (defparameter *bigger-grammar*
@@ -167,30 +141,3 @@
 
 
 
-(defun mappend (fn the-list)
-  (apply #'append (mapcar fn the-list)))
-
-;;; exercise 2.4
-
-;; by my notion of x-product
-(defun x-product (fn v1 v2)
-  "my notion of x-product"
-  (mapcar #'(lambda (x) (mapcar #'(lambda (y) (funcall fn x y))
-                                v2))
-          v1))
-
-(defun combine-all (xlist ylist)
-  (apply #'append (x-product #'list xlist ylist)))
-
-
-;;by PAIP's notion of x-product
-
-(defun x-product (fn v1 v2)
-  "PAIP's notion of x-product"
-  (mappend #'(lambda (x) (mapcar #'(lambda (y) (funcall fn x y))
-                                v2))
-          v1))
-
-
-(defun combine-all (xlist ylist)
-  (x-product #'append xlist ylist))
